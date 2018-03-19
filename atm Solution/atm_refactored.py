8b620cd2767d4b7ad8ac3264522ab77248000d86
Nermine Salah <nermine_salah1992@yahoo.com>
2018-03-19 23:20:59 +0200

    new code for atm

---
+++ atm Solution/atm_refactored.py
@@ -0,0 +1,29 @@
+def withdraw(balance, request):
+    if request > balance:
+        print('Cant give you all this money')
+    elif request < 0:
+        print('More than Zero please!')
+    else:
+        while request > 0:
+            if request >= 100:
+                request = request - 100
+                print('give 100')
+            elif request >= 50:
+                request = request - 50
+                print('give 50')
+            elif request >= 10:
+                request = request - 10
+                print('give 10')
+            elif request >= 5:
+                request = request - 5
+                print('give 5')
+            elif request < 5:
+                print('give '+str(request))
+                request = 0
+    return balance - request
+
+balance = 500
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
