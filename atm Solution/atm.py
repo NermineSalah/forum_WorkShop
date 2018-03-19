42020f8a4d735a1efef01d96f446a9da66970df7
Nermine Salah <nermine_salah1992@yahoo.com>
2018-03-19 22:24:07 +0200

    Modifying solution

--- atm Solution/atm.py
+++ atm Solution/atm.py
@@ -1,22 +1,26 @@
 balance = 500
-request = 277
+request = 250
 def ATM(request):
-    while request > 0:
-        if request > 100:
-            print 'give 100'
-            request =100
-        elif request > 50:
-            print 'give 50'
-            request = 50
-        elif request > 10:
-            print 'give 10'
-            request = 10
-        elif request > 5:
-            print 'give 5'
-            request = 5
-        else:
-            if request == '0':
-                print 'give 0'
+    if request > balance:
+        print('Cant give you all this money')
+    elif request < 0:
+        print ('More than zero please')
+    else:
+        while request > 0:
+            if request >= 100:
+                request -=100
+                print 'give 100'
+            elif request >= 50:
+                request = -50
+                print 'give 50'
+            elif request >= 10:
+                request = -10
+                print 'give 10'
+            elif request >= 5:
+                request = -5
+                print 'give 5'
+            elif request < 5:
+                print ('give '+str(request))
                 request = 0
 
   
